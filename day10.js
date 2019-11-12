003eb2a671b07e5e0c7a90fd2997459342428b3d
Alice Lee <alicesmlee91@gmail.com>
2019-11-12 10:56:34 -0800

    fixed day 10

--- day10.js
+++ day10.js
@@ -1,29 +1,17 @@
 const whereCanIPark = (spots, vehicle) => {
-
+    // Code here!
     for (let y = 0; y < spots.length; y++) {
-        for (let x = 0; x < spots[y].length - 1; x++) {
-
-            const spot = spots[x][y];
-
-            //console.log([x][y])
-
-            if (vehicle === 'regular') {
-                if (spot === 'R') {
-                    return [y, x];
-                }
-
-            } else if (vehicle === 'small') {
-                if (spot === 'R' || spot === 'S') {
-                    return [y, x];
-                }
-
-            } else if (vehicle === 'motorcycle') {
-                if (spot === 'R' || spot === 'S' || spot === 'M') {
-                    return [y, x];
-                }
+        for (let x = 0; x < spots[y].length; x++) {
+            const spot = spots[y][x];
+            if (vehicle == "regular") {
+                if (spot == "R") return [x, y];
+            } else if (vehicle == "small") {
+                if (spot == "R" || spot == "S") return [x, y];
+            } else {
+                if (spot == "R" || spot == "S" || spot == "M")
+                    return [x, y];
             }
-
         }
     }
     return false;
-};
+}
